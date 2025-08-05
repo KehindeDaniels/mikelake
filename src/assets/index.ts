81bde820f4e1bdd55463cc5f43fa7d88a6e137c5
kehindedaniels <kehindedaniels501@gmail.com>
2025-08-05 18:35:01 +0100

    changed image dirctory

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,12 @@
-import pic1 from "./images/pic-1.jpg";
-import pic2 from "./images/pic-2.jpg";
+import pic1 from "./image/pic-1.jpg";
+import pic2 from "./image/pic-2.jpg";
 
-import pic4 from "./images/pic-4.jpg";
-import pic5 from "./images/pic-5.jpg";
-import pic6 from "./images/pic-6.jpg";
-import pic7 from "./images/pic-7.jpg";
+import pic4 from "./image/pic-4.jpg";
+import pic5 from "./image/pic-5.jpg";
+import pic6 from "./image/pic-6.jpg";
+import pic7 from "./image/pic-7.jpg";
 
-import profilePics from "./images/profilePics.jpg";
+import profilePics from "./image/profilePics.jpg";
 
 export const images = {
   pic1,
